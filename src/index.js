213c9f9d34f777f2bdffcefda44ebcb2b5a06318
lasernoy <cloudy.nny07@gmail.com>
2023-12-08 12:12:39 +0800

    App component: newer syntax

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// import './index.css';
+// import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,7 +11,30 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+/**
+  There two pieces of state:
+    1. random quote with author
+    2. random theme color to each random quote
+*/
+
+
+/* FCC Random Quote Machine */
+
+// REACT
+function App() {
+  return (
+    <section id='quote-box'>
+      <blockquote id='text'>
+        <span className='double-quote'>&quot;</span>
+        This is a random quote.
+      </blockquote>
+      <span id='author'>- Author</span>
+      <button id='new-quote'>New quote</button>
+      <a
+        id='tweet-quote'
+        href='twitter.com/intent/tweet'
+        target='_blank'
+      >Tweet</a>
+    </section>
+  );
+}
