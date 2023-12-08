633e8d8061e70d65267ed2de0816c382ffdc1d68
lasernoy <cloudy.nny07@gmail.com>
2023-12-08 19:01:40 +0800

    functionality is fine

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { useState } from 'react';
+
+import quotes from './quotes.js';
+
 import './index.css';
 import './styles.css'
 // import App from './App';
@@ -12,6 +16,7 @@ root.render(
   </React.StrictMode>
 );
 
+
 /**
   There are two pieces of state:
     1. random quote with author
@@ -21,16 +26,31 @@ root.render(
 
 /* FCC Random Quote Machine */
 
+const randomQuote = (q) => {
+  const random = Math.floor(Math.random() * q.length);
+  console.log(q.length, random);
+  return q[random];
+};
+randomQuote(quotes);
 // REACT
 function App() {
+  const [quote, setQuote] = useState(randomQuote(quotes));
+
+  function handleClick() {
+    setQuote(randomQuote(quotes));
+  }
+
   return (
     <section id='quote-box'>
       <blockquote id='text'>
         <span className='double-quote'>&quot;</span>
-        This is a random quote.
+        {quote['quote']}
       </blockquote>
-      <span id='author'>- Author</span>
-      <button id='new-quote'>New quote</button>
+      <span id='author'>- {quote['author']}</span>
+      <button
+        id='new-quote'
+        onClick={handleClick}
+      >New quote</button>
       <a
         id='tweet-quote'
         href='twitter.com/intent/tweet'
