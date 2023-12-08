81ae8eba39e930b80bbbcd759f07af543006eb90
lasernoy <cloudy.nny07@gmail.com>
2023-12-08 13:20:35 +0800

    imported stylesheet to component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
+import './index.css';
+import './styles.css'
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
@@ -12,7 +13,7 @@ root.render(
 );
 
 /**
-  There two pieces of state:
+  There are two pieces of state:
     1. random quote with author
     2. random theme color to each random quote
 */
