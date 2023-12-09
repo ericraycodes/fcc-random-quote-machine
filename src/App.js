308437a4e3d896f401cd34ccfff3651a67e103e2
lasernoy <cloudy.nny07@gmail.com>
2023-12-09 22:13:21 +0800

    I transferred the <App/> component from index.sj

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { useState } from 'react';
 
-function App() {
+import quotes from './quotes.js';
+
+import './styles.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons';
+
+// Resource for random color options.
+const colors = [
+  '#711DB0', '#FFB534', '#88AB8E', '#1640D6', '#3081D0',
+  '#994D1C', '#B06161', '#A25772', '#FF5B22', '#B0A695'
+];
+
+// The functions generate random quotes and colors.
+const generateRandomQuote = (q) => q[Math.floor(Math.random() * q.length)];
+const generateRandomColor = (c) => c[Math.floor(Math.random() * c.length)];
+
+export default function App() {
+  const [quote, setQuote] = useState(generateRandomQuote(quotes));
+  const [color, setColor] = useState(generateRandomColor(colors));
+  const handleClick = () => {
+    setQuote(generateRandomQuote(quotes));
+    setColor(generateRandomColor(colors));
+  };
+  document.querySelector('body').style.backgroundColor = color;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <section id='quote-box'>
+      <QuoteAndAuthor
+        quote={quote}
+        color={color}
+      />
+      <Buttons
+        quote={quote}
+        onClick={handleClick}
+        color={color}
+      />
+    </section>
+  );
+}
+
+function QuoteAndAuthor({ quote, color }) {
+  return (
+    <>
+      <div style={{color: color}}>
+        <FontAwesomeIcon className='icon-quote' icon={faQuoteLeft} />
+        <blockquote id='text'>{quote['quote']}</blockquote>
+      </div>
+      <span id='author' style={{color: color}}>- {quote['author']}</span>
+    </>
+  );
+}
+
+function Buttons({ quote, onClick, color }) {
+  // The 'href' must be completed for tweeting the fetched quote.
+  return (
+    <div className='buttons'>
+      <a
+        id='tweet-quote'
+        href={'twitter.com/intent/tweet' + quote }
+        target='_blank'
+        style={{backgroundColor: color}}
+      ><FontAwesomeIcon icon={faXTwitter} /></a>
+      <button
+        id='new-quote'
+        onClick={onClick}
+        style={{backgroundColor: color}}
+      >New quote</button>
     </div>
   );
 }
 
-export default App;
+export function Footer() {
+  return (
+    <div className='footer'>
+      <p>By Eric Ray O. Saladar. For freeCodeCamp purposes only.</p>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { useState } from 'react';
 
-import quotes from './quotes.js';
+import App from './App';
+import { Footer } from './App';
 
-// import './index.css';
-import './styles.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faXTwitter } from '@fortawesome/free-brands-svg-icons';
-import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -17,71 +12,3 @@ root.render(
     <Footer />
   </React.StrictMode>
 );
-
-const colors = ['gray', 'brown', 'yellow', 'blue', 'orange'];
-
-const generateRandomQuote = (q) => q[Math.floor(Math.random() * q.length)];
-const generateRandomColor = (c) => c[Math.floor(Math.random() * c.length)];
-
-// REACT
-function App() {
-  const [quote, setQuote] = useState(generateRandomQuote(quotes));
-  const [color, setColor] = useState(generateRandomColor(colors))
-  const handleClick = () => {
-    setQuote(generateRandomQuote(quotes));
-    setColor(generateRandomColor(colors));
-  };
-  document.querySelector('body').style.backgroundColor = color;
-  return (
-    <section id='quote-box'>
-      <QuoteAndAuthor
-        quote={quote}
-        color={color}
-      />
-      <Buttons
-        onClick={handleClick}
-        color={color}
-      />
-    </section>
-  );
-}
-
-function QuoteAndAuthor({ quote, color }) {
-  return (
-    <>
-      <div style={{color: color}}>
-        <FontAwesomeIcon className='icon-quote' icon={faQuoteLeft} />
-        <blockquote id='text'>{quote['quote']}</blockquote>
-      </div>
-      <span id='author' style={{color: color}}>- {quote['author']}</span>
-    </>
-  );
-}
-
-function Buttons({ onClick, color }) {
-  return (
-    <div className='buttons'>
-      <a
-        id='tweet-quote'
-        href='twitter.com/intent/tweet'
-        target='_blank'
-        style={{backgroundColor: color}}
-      ><FontAwesomeIcon icon={faXTwitter} /></a>
-      <button
-        id='new-quote'
-        onClick={onClick}
-        style={{backgroundColor: color}}
-      >New quote</button>
-    </div>
-  );
-}
-
-function Footer() {
-  return (
-    <div className='footer'>
-      <p>By Eric Ray O. Saladar. For freeCodeCamp purposes only.</p>
-    </div>
-  );
-}
-
-const ref ="https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=%22Start%20where%20you%20are.%20Use%20what%20you%20have.%20%20Do%20what%20you%20can.%22%20Arthur%20Ashe";
